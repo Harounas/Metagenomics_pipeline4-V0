6e63acd6da9f4136dc2e9983ef3a364d8a009b52
Harouna Soumare <140737983+Harounas@users.noreply.github.com>
2025-04-05 12:35:11 +0100

    Update kraken_abundance_pipeline.py

--- Metagenomics_pipeline4_V0/kraken_abundance_pipeline.py
+++ Metagenomics_pipeline4_V0/kraken_abundance_pipeline.py
@@ -11,6 +11,7 @@ from .kraken2 import run_kraken2
 import distinctipy
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
 
 def process_sample(forward, reverse, base_name, bowtie2_index, kraken_db, output_dir, threads, run_bowtie, use_precomputed_reports, use_assembly):
     """
